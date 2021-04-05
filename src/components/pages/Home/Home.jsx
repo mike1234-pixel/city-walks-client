bc9a4a036c49a179259b8627774939b6fe47995a
Mike Tandy <miketandy@buttys-mac.home>
2021-04-05 18:51:29 +0100

    making homepage sections into links.

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -36,29 +36,35 @@ const Home = (props) => {
                     <h2 className="page-heading"><a href="#featured-walk-1" className="featured-walks-link"><FaArrowAltCircleDown className="arrow" /> Featured Walks <FaArrowAltCircleDown className="arrow"/></a></h2>
                 </div>
                 <hr/>
+                <Link to={"/walks/"+(urlify(featuredWalks[0].walk))}>
                 <div className="center featured-walks-h3-container" id="featured-walk-1">
                     <h3 className="featured-walks-h3">{featuredWalks[0].walk}</h3>
                 </div>
                 <SectionA content={featuredWalks[0].content1} img={featuredWalks[0].coverImg} alt={featuredWalks[0].walk}/>
-                <div className="center">
-                    <Link to={"/walks/"+(urlify(featuredWalks[0].walk))}><MDBBtn data-testid="explore-btn-1" outline color="elegant" className="featured-walks-btn">Explore {featuredWalks[0].walk} <MDBIcon icon="walking" /></MDBBtn></Link>
-                </div>
+                    <div className="center">
+                        <MDBBtn data-testid="explore-btn-1" outline color="elegant" className="featured-walks-btn">Explore {featuredWalks[0].walk} <MDBIcon icon="walking" /></MDBBtn>
+                    </div>
+                </Link>
                 <hr/>
+                <Link to={"/walks/"+(urlify(featuredWalks[1].walk))}>
                 <div className="center featured-walks-h3-container">
                     <h3 className="featured-walks-h3">{featuredWalks[1].walk}</h3>
                 </div>
                 <SectionB content={featuredWalks[1].content1} img={featuredWalks[1].coverImg} alt={featuredWalks[1].walk}/>
-                <div className="center">
-                    <Link to={"/walks/"+(urlify(featuredWalks[1].walk))}><MDBBtn data-testid="explore-btn-2" className="section-b-home-btn" outline color="elegant" className="featured-walks-btn">Explore {featuredWalks[1].walk} <MDBIcon icon="walking" /></MDBBtn></Link>
-                </div>
+                    <div className="center">
+                        <MDBBtn data-testid="explore-btn-2" className="section-b-home-btn" outline color="elegant" className="featured-walks-btn">Explore {featuredWalks[1].walk} <MDBIcon icon="walking" /></MDBBtn>
+                    </div>
+                </Link>
                 <hr/>
-                <div className="center featured-walks-h3-container">
-                    <h3 className="featured-walks-h3">{featuredWalks[2].walk}</h3>
-                </div>
+                <Link to={"/walks/"+(urlify(featuredWalks[2].walk))}>
+                    <div className="center featured-walks-h3-container">
+                        <h3 className="featured-walks-h3">{featuredWalks[2].walk}</h3>
+                    </div>
                 <SectionA content={featuredWalks[2].content1} img={featuredWalks[2].coverImg} alt={featuredWalks[2].walk}/>
                 <div className="center">
-                    <Link to={"/walks/"+(urlify(featuredWalks[2].walk))}><MDBBtn data-testid="explore-btn-3" outline color="elegant" className="featured-walks-btn">Explore {featuredWalks[2].walk} <MDBIcon icon="walking" /></MDBBtn></Link>
+                    <MDBBtn data-testid="explore-btn-3" outline color="elegant" className="featured-walks-btn">Explore {featuredWalks[2].walk} <MDBIcon icon="walking" /></MDBBtn>
                 </div>
+                </Link>
             </div>
         </MDBContainer>
     )
